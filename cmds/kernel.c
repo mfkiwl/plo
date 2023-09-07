b3f1fcd32f41c08120670e1044d4d41785e1aeaa
lukileczo <lukasz.leczkowski@phoenix-rtos.com>
2023-09-07 16:40:49 +0200

    cmd_kernel: add support for 64-bit binaries
    JIRA: RTOS-550

--- cmds/kernel.c
+++ cmds/kernel.c
@@ -28,6 +28,9 @@ static void cmd_kernelInfo(void)
 }
 
 
+#ifndef __TARGET_RISCV64
+
+
 static int cmd_kernel(int argc, char *argv[])
 {
 	u8 buff[SIZE_MSG_BUFF];
@@ -108,6 +111,99 @@ static int cmd_kernel(int argc, char *argv[])
 
 	return EOK;
 }
+
+
+#else
+
+
+static int cmd_kernel(int argc, char *argv[])
+{
+	u8 buff[SIZE_MSG_BUFF];
+	ssize_t res;
+	addr_t kernelPAddr = (addr_t)-1;
+	const char *kname;
+	handler_t handler;
+
+	size_t elfOffs = 0, segOffs;
+
+	Elf64_Word i;
+	Elf64_Ehdr hdr;
+	Elf64_Phdr phdr;
+
+	const mapent_t *entry;
+
+	/* Parse arguments */
+	if (argc == 1 || argc > 3) {
+		log_error("\n%s: Wrong argument count", argv[0]);
+		return -EINVAL;
+	}
+
+	kname = (argc == 3) ? argv[2] : PATH_KERNEL;
+
+	res = phfs_open(argv[1], kname, 0, &handler);
+	if (res < 0) {
+		log_error("\nCannot open %s, on %s", kname, argv[1]);
+		return res;
+	}
+
+	/* Read ELF header */
+	res = phfs_read(handler, elfOffs, &hdr, sizeof(Elf64_Ehdr));
+	if (res < 0) {
+		log_error("\nCan't read %s, on %s", kname, argv[1]);
+		return res;
+	}
+
+	if ((hdr.e_ident[0] != 0x7f) || (hdr.e_ident[1] != 'E') || (hdr.e_ident[2] != 'L') || (hdr.e_ident[3] != 'F')) {
+		log_error("\n%s isn't an ELF object", kname);
+		return -EINVAL;
+	}
+
+	/* Read program segments */
+	for (i = 0; i < hdr.e_phnum; i++) {
+		elfOffs = hdr.e_phoff + i * sizeof(Elf64_Phdr);
+		res = phfs_read(handler, elfOffs, &phdr, sizeof(Elf64_Phdr));
+		if (res < 0) {
+			log_error("\nCan't read %s, on %s", kname, argv[1]);
+			return res;
+		}
+
+		if ((phdr.p_type == (Elf64_Word)PHT_LOAD)) {
+			entry = syspage_entryAdd(NULL, hal_kernelGetAddress((addr_t)phdr.p_vaddr), phdr.p_memsz, phdr.p_align);
+			if (entry == NULL) {
+				log_error("\nCannot allocate memory for '%s'", kname);
+				return -ENOMEM;
+			}
+
+			/* Save kernel's beginning address */
+			if (phdr.p_flags == (Elf64_Word)(PHF_R | PHF_X)) {
+				kernelPAddr = entry->start;
+			}
+
+			elfOffs = phdr.p_offset;
+
+			for (segOffs = 0; segOffs < phdr.p_filesz; elfOffs += res, segOffs += res) {
+				res = phfs_read(handler, elfOffs, buff, min(sizeof(buff), phdr.p_filesz - segOffs));
+				if (res < 0) {
+					log_error("\nCan't read %s, on %s", kname, argv[1]);
+					return res;
+				}
+
+				hal_memcpy((void *)(entry->start + segOffs), buff, res);
+			}
+		}
+	}
+
+	hal_kernelEntryPoint(hal_kernelGetAddress(hdr.e_entry));
+	syspage_kernelPAddrAdd(kernelPAddr);
+	phfs_close(handler);
+
+	log_info("\nLoaded %s", kname);
+
+	return EOK;
+}
+
+
+#endif /* __TARGET_RISCV64 */
 
 
 __attribute__((constructor)) static void cmd_kernelReg(void)
